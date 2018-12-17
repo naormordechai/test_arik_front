1a5860d8b3196a7c3a3b881b72a0613e7322caf1
unknown <naormordechaix@gmail.com>
2018-12-17 19:47:35 +0200

    added creation date

--- src/components/RecordList/RecordList.js
+++ src/components/RecordList/RecordList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import RecordPreview from '../RecordPreview/RecordPreview'
 
-const recordList = ({records}) => (
+const recordList = ({ records }) => (
     <div>
         {records.map(record => (
-            <div style={{display:'flex', alignItems:'center'}} key={record._id}>
-                <RecordPreview record={record}/>
-                <div> time: {record.stopTimestamp - record.startTimestamp} ms</div> 
+            <div style={{ display: 'flex', alignItems: 'center', marginBottom:'10px' }} key={record._id}>
+                <RecordPreview record={record} />
+                <div> time: {record.stopTimestamp - record.startTimestamp} ms |
+                 creation deta: {new Date(record.startTimestamp).toDateString()}</div>
             </div>
         ))}
     </div>
--- src/services/RecordService.js
+++ src/services/RecordService.js
@@ -1,13 +1,12 @@
 import axios from 'axios'
-
 const RECORD_URL = (process.env.NODE_ENV !== 'development')
     ? '/record'
     : '//localhost:8080/record';
 
-    async function query() {
-        return await axios.get(RECORD_URL)
-    }
+async function query() {
+    return await axios.get(RECORD_URL)
+}
 
-    export default {
-        query
-    }
+export default {
+    query
+}
